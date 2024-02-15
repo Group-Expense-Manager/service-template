2b5db58d9fce1b6500d03a23bccd9c2853c8ae9e
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-02-15 18:44:19 +0100

    added ticket validation

--- .github/actions/jira-ticket-validator.js
+++ .github/actions/jira-ticket-validator.js
@@ -4,12 +4,12 @@ function validateJiraTicket(jiraTicketRegex, input, fieldName) {
     if (jiraTicketRegex.test(input)) {
         console.log(`${fieldName} is valid.`);
     } else {
-        console.error(`${fieldName} is invalid. It should have a Jira ticket number in the format "HS-21". (${input})`);
+        console.error(`${fieldName} is invalid. It should have a Jira ticket number in the format "GEM-21". (${input})`);
         core.setFailed('Jira ticket number validation failed.');
     }
 }
 
-const jiraTicketRegex = /^HS-\d+(\s\|\s\w.*)?$/;
+const jiraTicketRegex = /^GEM-\d+(\s\|\s\w.*)?$/;
 
 const branchName = process.env.GITHUB_HEAD_REF || '';
 const prTitle = process.argv[2];
