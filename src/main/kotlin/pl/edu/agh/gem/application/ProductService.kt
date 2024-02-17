f408d15cca976af0d519857cfdbd916a8c1f89a3
Paweł Gacek <pawelgacek.pl@gmail.com>
2024-02-17 08:28:39 +0100

    some changes

--- src/main/kotlin/pl/edu/agh/gem/application/ProductService.kt
+++ src/main/kotlin/pl/edu/agh/gem/application/ProductService.kt
@@ -1,14 +1,20 @@
 package pl.edu.agh.gem.application
 
 import org.springframework.stereotype.Service
+import pl.edu.agh.gem.domain.Product
 import pl.edu.agh.gem.infrastructure.persistence.ProductRepository
+import pl.edu.agh.gem.interfaces.rest.model.ProductResponse
 import reactor.core.publisher.Flux
 
 @Service
 class ProductService(
     val productRepository: ProductRepository,
 ) {
-    fun getAllNames(): Flux<String> {
-        return productRepository.findAll().map { product -> product.name }
+    fun getAll(): Flux<ProductResponse> {
+        return productRepository.findAll().map { product -> mapToProductResponse(product) }
+    }
+
+    private fun mapToProductResponse(product: Product): ProductResponse {
+        return ProductResponse(product.name)
     }
 }
---
+++ src/main/kotlin/pl/edu/agh/gem/interfaces/rest/model/ProductResponse.kt
@@ -0,0 +1,5 @@
+package pl.edu.agh.gem.interfaces.rest.model
+
+data class ProductResponse(
+    val name: String,
+)
