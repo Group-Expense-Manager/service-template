838080a6f13441b5f96022ff5aae02b6095cdd46
Jakub Spolnik <lawos231@gmail.com>
2024-03-25 00:26:58 +0100

    GEM-7 | fix port

--- src/integration/kotlin/pl/edu/agh/gem/integration/ability/ServiceTestClient.kt
+++ src/integration/kotlin/pl/edu/agh/gem/integration/ability/ServiceTestClient.kt
@@ -2,10 +2,9 @@ package pl.edu.agh.gem.integration.ability
 
 import org.springframework.context.annotation.Lazy
 import org.springframework.http.HttpHeaders.ACCEPT
-import org.springframework.http.HttpHeaders.CONTENT_TYPE
 import org.springframework.stereotype.Component
 import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec
-import org.springframework.test.web.servlet.client.MockMvcWebTestClient
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient.bindToApplicationContext
 import org.springframework.web.context.WebApplicationContext
 import pl.edu.agh.gem.external.dto.product.ProductRequest
 import pl.edu.agh.gem.external.utils.InternalApiMediaType.APPLICATION_JSON_INTERNAL_VER_1
@@ -13,7 +12,7 @@ import java.net.URI
 @Component
 @Lazy
 class ServiceTestClient(applicationContext: WebApplicationContext) {
-    private val webClient = MockMvcWebTestClient.bindToApplicationContext(applicationContext)
+    private val webClient = bindToApplicationContext(applicationContext)
         .configureClient()
         .build()
 
@@ -27,7 +26,6 @@ class ServiceTestClient(applicationContext: WebApplicationContext) {
     fun createProduct(body: ProductRequest): ResponseSpec {
         return webClient.post()
             .uri(URI("/api/products"))
-            .header(CONTENT_TYPE, APPLICATION_JSON_INTERNAL_VER_1)
             .header(ACCEPT, APPLICATION_JSON_INTERNAL_VER_1)
             .bodyValue(body)
             .exchange()
