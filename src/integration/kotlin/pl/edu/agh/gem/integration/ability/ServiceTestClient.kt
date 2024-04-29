772a2e1fb6abde9a598d0e84daf412c5f6a41545
Jakub Spolnik <lawos231@gmail.com>
2024-04-29 18:40:15 +0200

    GEM-56 | Rename functions

--- src/integration/kotlin/pl/edu/agh/gem/integration/ability/ServiceTestClient.kt
+++ src/integration/kotlin/pl/edu/agh/gem/integration/ability/ServiceTestClient.kt
@@ -6,9 +6,9 @@ import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec
 import org.springframework.test.web.servlet.client.MockMvcWebTestClient.bindToApplicationContext
 import org.springframework.web.context.WebApplicationContext
 import pl.edu.agh.gem.external.dto.product.ProductRequest
-import pl.edu.agh.gem.headers.HeadersUtils.addAppAcceptType
-import pl.edu.agh.gem.headers.HeadersUtils.addAppContentType
-import pl.edu.agh.gem.headers.HeadersUtils.addValidatedUser
+import pl.edu.agh.gem.headers.HeadersUtils.withAppAcceptType
+import pl.edu.agh.gem.headers.HeadersUtils.withAppContentType
+import pl.edu.agh.gem.headers.HeadersUtils.withValidatedUser
 import pl.edu.agh.gem.security.GemUser
 import java.net.URI
 
@@ -22,14 +22,14 @@ class ServiceTestClient(applicationContext: WebApplicationContext) {
     fun findProduct(id: String, user: GemUser): ResponseSpec {
         return webClient.get()
             .uri(URI("/api/products/$id"))
-            .headers { it.addAppAcceptType().addValidatedUser(user) }
+            .headers { it.withAppAcceptType().withValidatedUser(user) }
             .exchange()
     }
 
     fun createProduct(body: ProductRequest): ResponseSpec {
         return webClient.post()
             .uri(URI("/api/products"))
-            .headers { it.addAppContentType() }
+            .headers { it.withAppContentType() }
             .bodyValue(body)
             .exchange()
     }
